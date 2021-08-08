c0ff27e4d4ba4c62ac55f471a9630bff8024520d
tiendat.nt <good.engineer810@gmail.com>
2021-08-08 12:12:49 +0900

    1351. Count Negative Numbers in a Sorted Matrix 1. Idea 1: brute-force - visit all elements in the matrix m x n - for each time of visiting, counting the negative number if the number is less than 0 2. Evaluation - Time Complexity: O(N^2) - Space Complexity: O(1) - Code Complexity: simple

--- leetcode/1351_Count_Negative_Numbers_in_a_Sorted_Matrix/solution.cpp
+++ leetcode/1351_Count_Negative_Numbers_in_a_Sorted_Matrix/solution.cpp
@@ -1,6 +1,18 @@
 class Solution {
+private:
+    int firstSolution(vector<vector<int>>& grid) {
+        int negativeNumberCnts = 0;
+        for (int r = 0, m = grid.size(); r < m; ++r) {
+            for (int c = 0, n = grid[r].size(); c < n; ++c) {
+                if (grid[r][c] >= 0) continue;
+                ++negativeNumberCnts;
+            }
+        }
+        return negativeNumberCnts;
+    }
+
 public:
     int countNegatives(vector<vector<int>>& grid) {
-        
+        return firstSolution(grid);
     }
 };
