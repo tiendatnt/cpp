bd091e353372a74a780aacc3e9017a7a4ddf519e
tiendat.nt <good.engineer810@gmail.com>
2021-08-08 17:05:06 +0900

    1351. Count Negative Numbers in a Sorted Matrix 1. Idea 4: recursion - the non-negative rows and non-negative columns are not needed to consider - calculate the number of negative elements in the negative rows and negative columns of the current matrix - the final result will be the sum of the result in the above caculation and the number of negative elements in the smaller matrix (by removing the non-negative rows, negative rows, non-negative columns, negative columns) 2. Evaluation - Time Complexity: O(m + n) in bad case (every rows and columns contain both negative numbers and non-negative numbers). Here, m is the number of rows and n is the number of columns of the matrix. - Space Complexity: O(m + n) - Code Complexity: complex

--- leetcode/1351_Count_Negative_Numbers_in_a_Sorted_Matrix/solution.cpp
+++ leetcode/1351_Count_Negative_Numbers_in_a_Sorted_Matrix/solution.cpp
@@ -52,10 +52,36 @@ private:
         return negativeNumberCnts;
     }
 
+    int fourthSolution(vector<vector<int>>& grid, int curMs, int curMe, int curNs, int curNe) {
+        if (grid[curMs][curNs] < 0)
+            return (curMe - curMs) * (curNe - curNs);
+
+        if (grid[curMe - 1][curNe - 1] >= 0)
+            return 0;
+
+        int nextMs = curMs;
+        for (int r = curMs; r < curMe && grid[r][curNe - 1] >= 0; nextMs = ++r);
+
+        int nextNs = curNs;
+        for (int c = curNs; c < curNe && grid[curMe - 1][c] >= 0; nextNs = ++c);
+
+        int nextMe = curMe;
+        for (int r = curMe; r > curMs && grid[r - 1][curNs] < 0; nextMe = --r);
+
+        int nextNe = curNe;
+        for (int c = curNe; c > curNs && grid[curMs][c - 1] < 0; nextNe = --c);
+
+        return (curNe - nextNe) * (curMe - curMs) +
+            (curMe - nextMe) * (curNe - curNs) -
+            (curMe - nextMe) * (curNe - nextNe) +
+            fourthSolution(grid, nextMs, nextMe, nextNs, nextNe);
+    }
+
 public:
     int countNegatives(vector<vector<int>>& grid) {
         //return firstSolution(grid);
         //return secondSolution(grid);
-        return thirdSolution(grid);
+        //return thirdSolution(grid);
+        return fourthSolution(grid, 0, grid.size(), 0, grid[0].size());
     }
 };
