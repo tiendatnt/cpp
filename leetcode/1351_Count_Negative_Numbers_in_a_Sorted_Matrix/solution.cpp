c462b34e3bbc7a4cb486aa527d6bb3ffbaeee15e
tiendat.nt <good.engineer810@gmail.com>
2021-08-08 14:05:19 +0900

    1351. Count Negative Numbers in a Sorted Matrix 1. Idea 3: use binary search - for each row of the matrix, find the first element is less than 0. - if grid[r][c] is the first element is less than 0 in the r-th row, all elements from r-th row and c-th column are also less than 0. From here, the elements in the left of column c are needed to be checked. 2. Evaluation - Time Complexity: O(mlogn) in the normal cases, O(m) in the best case (all elements are greater than 0). Here, m is the number of rows and n is the number of columns of the matrix. - Space Complexity: O(1) - Code Complexity: complex

--- leetcode/1351_Count_Negative_Numbers_in_a_Sorted_Matrix/solution.cpp
+++ leetcode/1351_Count_Negative_Numbers_in_a_Sorted_Matrix/solution.cpp
@@ -26,9 +26,36 @@ private:
         return negativeNumberCnts;
     }
 
+    int thirdSolution(vector<vector<int>>& grid) {
+        int negativeNumberCnts = 0;
+        int m = grid.size(), n = grid[0].size();
+
+        for (int row = 0; row < m; ++row) {
+            if (n < 1) break;
+            int right = n - 1;
+            if (grid[row][right] >= 0) continue;
+
+            int left = 0;
+            while (left <= right) {
+                if (grid[row][left] < 0) break;
+
+                int med = (left + right) / 2;
+                if (grid[row][med] < 0)
+                    right = med - 1;
+                else
+                    left = med + 1;
+            }
+
+            negativeNumberCnts += ((n - left) * (m - row));
+            n = left;
+        }
+        return negativeNumberCnts;
+    }
+
 public:
     int countNegatives(vector<vector<int>>& grid) {
         //return firstSolution(grid);
-        return secondSolution(grid);
+        //return secondSolution(grid);
+        return thirdSolution(grid);
     }
 };
