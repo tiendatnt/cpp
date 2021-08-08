a6500c044c2fe8135a4c02b0477c8d642f480a17
tiendat.nt <good.engineer810@gmail.com>
2021-08-08 22:27:22 +0900

    1688. Count of Matches in Tournament 1. Idea 1: Recursion - number of Matches = the matches in the current round + number of Matches in next round - the matches in the current round = n / 2 (n is the number of teams in the current round) - the number of teams in the next round = n / 2 + n % 2 2. Evaluation - Time Complexity: O(n) - Space Complexity: O(logn) - Code Complexity: simple

--- leetcode/1688_Count_of_Matches_in_Tournament/solution.cpp
+++ leetcode/1688_Count_of_Matches_in_Tournament/solution.cpp
@@ -1,6 +1,14 @@
 class Solution {
+private:
+    int firstSolution(int n) {
+        if (n <= 2)
+            return n/2;
+
+        int currRoundMatchesCnts = n / 2;
+        return currRoundMatchesCnts + numberOfMatches(currRoundMatchesCnts + n % 2);
+    }
 public:
     int numberOfMatches(int n) {
-        
+        return firstSolution(n);
     }
 };
