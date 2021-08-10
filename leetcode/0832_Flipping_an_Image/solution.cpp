9ceb90d5d426837f31a04ba34628cb996d941426
tiendat.nt <good.engineer810@gmail.com>
2021-08-10 15:11:22 +0900

    832. Flipping an Image 1. Idea 1: brute-force optimization - if image[i] and image[N-1-i] are different, they are still no changes after the image is reversed and inverted. - if image[i] and image[N-1-i] are same, they are invert after the image is reversed and inverted 2. Evaluation - Time Complexity: O(N^2) - Space Complexity: O(1) - Code Complexity: simmple Here, N is the number of row of the image.

--- leetcode/0832_Flipping_an_Image/solution.cpp
+++ leetcode/0832_Flipping_an_Image/solution.cpp
@@ -1,6 +1,20 @@
 class Solution {
+private:
+    vector<vector<int>> firstSolution(vector<vector<int>>& image) {
+        int N = image.size();
+        for (int row = 0; row < N; ++row) {
+            for (int i = 0, j = N - 1; i <= j; ++i, --j) {
+                if (image[row][i] == image[row][j]) {
+                    image[row][j] = 1 - image[row][j];
+                    image[row][i] = image[row][j];
+                }
+            }
+        }
+        return image;
+    }
+
 public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
-        
+        return firstSolution(image);
     }
 };
