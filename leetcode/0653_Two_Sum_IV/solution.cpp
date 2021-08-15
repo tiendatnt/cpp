138deb245dc9d928334367b0fdc560454d5f849e
Nguyen Tien Dat <tiendat.nt810@gmail.com>
2021-08-15 17:15:07 +0900

    653. Two Sum IV - Input is a BST 1. Idea 1: convert the binary search tree to a increasing vector - convert the binary search tree to a increasing vector (using recursion implementation) - using 2 pointers to check if there are 2 numbers in the vector that sum of them equal target k
    2. Evaluation
    - Time Complexity: O(N)
    - Space Complexity: O(logN)
    - Code Complexity: medium
    Here, N is the number of nodes in the tree

--- leetcode/0653_Two_Sum_IV/solution.cpp
+++ leetcode/0653_Two_Sum_IV/solution.cpp
@@ -10,8 +10,36 @@
  * };
  */
 class Solution {
+private:
+    void createIncVector(TreeNode* root, vector<int>& outVect) {
+        if(!root)
+            return;
+
+        createIncVector(root->left, outVect);
+        outVect.push_back(root->val);
+        createIncVector(root->right, outVect);
+    }
+    bool firstSolution(TreeNode* root, int k) {
+        vector<int> incVect;
+        createIncVector(root, incVect);
+        for (int l = 0, r = incVect.size() - 1; l < r; ) {
+            if (incVect[l] + incVect[r] > k) {
+                --r;
+                continue;
+            }
+
+            if (incVect[l] + incVect[r] < k) {
+                ++l;
+                continue;
+            }
+
+            return true;
+        }
+        return false;
+    }
+
 public:
     bool findTarget(TreeNode* root, int k) {
-        
+        return firstSolution(root, k);
     }
 };
